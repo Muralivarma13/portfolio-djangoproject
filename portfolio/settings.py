d60d063b627e0129c6c9bd032d24746716a13f90
Muralivarma13 <muralitirumani117@gmail.com>
2026-01-29 18:40:49 +0530

    deploy

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'resume',
-    'contact',
+    
    
 ]
 
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'portfolio_db',
-        'USER': 'root',
-        'PASSWORD': '123456',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -20,7 +20,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("resume.urls")),
-    path('contact/', include('contact.urls')),
+   
 
    
 ]
